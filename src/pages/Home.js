51f73f1b7fe92f8d585c1351450d056de8556b5e
Steven Yang <steventanyang@gmail.com>
2024-07-13 20:14:16 -0400

    udder

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,90 @@
 import styled from "styled-components";
 
-const Main = styled.div`
-  background-color: "E1E1E1";
-`;
-
 const Overlay = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background-color: #e1e1e1;
   z-index: 0;
-  overflow: scroll;
+  overflow: hidden;
+  scroll
 `;
+
+const DrippingEffect = styled.div`
+  position: relative;
+  width: 100%;
+  height: 50%;
+  background-color: #f4a5b7;
+  border-bottom-left-radius: 50% 100%;
+  border-bottom-right-radius: 50% 100%;
+`;
+
+const Text = styled.h1`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 2.5rem;
+  color: #4b2e59;
+`;
+
+
+const UdderContainer = styled.div`
+  z-index: 10000;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: space-evenly;
+  width: 100%;
+  height: 100%;
+  flex-direction: row;
+`;
+
+const TestUdder = styled.div`
+  width: 7%;
+  height: ${(props) => props.height || "50%"};
+  background-color: #DDA5B2;
+  border-radius: 100px;
+`;
+
+const UdderBodyContainer = styled.div`
+  position: absolute;
+  display: flex;
+  width: 100%;
+  height: 50%;
+  justify-content: flex-start;
+`
+
+const UdderBody = styled.div`
+  width: 100%;
+  height: 50%;
+  background-color: #DDA5B2;
+  border-bottom-left-radius: 50% 100%;
+  border-bottom-right-radius: 50% 100%;
+  z-index: 10000;
+`
 
 const Home = () => {
   return (
-    <Main>
-      <Overlay>
-        <div>testing!!</div>
-      </Overlay>
-    </Main>
+    <Overlay>
+
+      <UdderBodyContainer>
+        <UdderBody />
+      </UdderBodyContainer>
+
+      <UdderContainer>
+        <TestUdder height="35%" />
+        <TestUdder height="45%" />
+        <TestUdder height="55%" />
+        <TestUdder height="45%" />
+        <TestUdder height="35%" />
+      </UdderContainer>
+
+
+
+    </Overlay>
   );
-}
+};
 
 export default Home;
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,28 +1,78 @@
 import styled from "styled-components";
 
-const Main = styled.div`
-  background-color: "E1E1E1";
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #d3d3d3;
+  overflow: hidden;
 `;
 
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: #e1e1e1;
-  z-index: 0;
-  overflow: scroll;
+const DrippingEffect = styled.div`
+  position: relative;
+  width: 100%;
+  height: 50%;
+  background-color: #f4a5b7;
+  border-bottom-left-radius: 50% 20%;
+  border-bottom-right-radius: 50% 20%;
+`;
+
+const Text = styled.h1`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 2.5rem;
+  color: #4b2e59;
+`;
+
+const Drip = styled.div`
+  position: absolute;
+  bottom: -25px;
+  width: 40px;
+  height: 100px;
+  background-color: #f4a5b7;
+  border-radius: 50%;
+  left: ${props => props.left};
+  animation: drip 3s infinite ease-in-out;
+
+  @keyframes drip {
+    0% {
+      height: 100px;
+    }
+    50% {
+      height: 120px;
+    }
+    100% {
+      height: 100px;
+    }
+  }
+
+  &::before {
+    content: '*';
+    position: absolute;
+    top: -25px;
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: 1.5rem;
+    color: white;
+  }
 `;
 
 const Project = () => {
   return (
-    <Main>
-      <Overlay>
-        <div>testing!!</div>
-      </Overlay>
-    </Main>
+    <Container>
+      <DrippingEffect>
+        <Text>BOFIND</Text>
+        <Drip left="20%" />
+        <Drip left="40%" />
+        <Drip left="60%" />
+        <Drip left="80%" />
+      </DrippingEffect>
+    </Container>
   );
-}
+};
+
 
 export default Project;
