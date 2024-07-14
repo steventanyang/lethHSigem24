afc2b84fddca89209b34a0e8e42ca96ce1543f2a
Steven Yang <steventanyang@gmail.com>
2024-07-13 20:30:20 -0400

    added jiggle animation on click

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
-import styled from "styled-components";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useSpring, animated } from '@react-spring/web';
 
 const Overlay = styled.div`
   position: fixed;
@@ -9,27 +11,7 @@ const Overlay = styled.div`
   background-color: #e1e1e1;
   z-index: 0;
   overflow: hidden;
-  scroll
 `;
-
-const DrippingEffect = styled.div`
-  position: relative;
-  width: 100%;
-  height: 50%;
-  background-color: #f4a5b7;
-  border-bottom-left-radius: 50% 100%;
-  border-bottom-right-radius: 50% 100%;
-`;
-
-const Text = styled.h1`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 2.5rem;
-  color: #4b2e59;
-`;
-
 
 const UdderContainer = styled.div`
   z-index: 10000;
@@ -41,48 +23,77 @@ const UdderContainer = styled.div`
   flex-direction: row;
 `;
 
-const TestUdder = styled.div`
+const StyledTestUdder = styled(animated.div)`
   width: 7%;
-  height: ${(props) => props.height || "50%"};
-  background-color: #DDA5B2;
+  height: ${(props) => props.height || '50%'};
+  background-color: #dda5b2;
   border-radius: 100px;
+  transition: transform 0.3s ease; /* Smooth transition */
+  z-index: 1;
+  &:hover {
+    transform: translateY(70px); /* Translate down on hover */
+  }
 `;
 
 const UdderBodyContainer = styled.div`
   position: absolute;
   display: flex;
   width: 100%;
-  height: 50%;
+  height: 100%;
   justify-content: flex-start;
-`
+`;
 
 const UdderBody = styled.div`
   width: 100%;
   height: 50%;
-  background-color: #DDA5B2;
+  background-color: #dda5b2;
   border-bottom-left-radius: 50% 100%;
   border-bottom-right-radius: 50% 100%;
   z-index: 10000;
-`
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  font-size: 7.5rem;
+  font-weight: 900;
+  color: #4b2e59;
+  margin-top: 5%;
+  z-index: 10000;
+`;
 
 const Home = () => {
+  const [jiggleIdx, setJiggleIdx] = useState(null);
+
+  const handleUdderClick = (idx) => {
+    setJiggleIdx(idx);
+    setTimeout(() => setJiggleIdx(null), 200); 
+  };
+
+  const jiggleAnimation = useSpring({
+    transform: jiggleIdx !== null ? 'translateY(70px) scale(1.5)' : 'translateY(70px) scale(1)',
+    config: { tension: 300, friction: 100, precision: 0.01 }
+  });
+
   return (
     <Overlay>
-
       <UdderBodyContainer>
-        <UdderBody />
+        <UdderBody>
+          <Title>BO FIND</Title>
+        </UdderBody>
       </UdderBodyContainer>
 
       <UdderContainer>
-        <TestUdder height="35%" />
-        <TestUdder height="45%" />
-        <TestUdder height="55%" />
-        <TestUdder height="45%" />
-        <TestUdder height="35%" />
+        {[55, 65, 75, 65, 55].map((height, idx) => (
+          <StyledTestUdder
+            key={idx}
+            style={jiggleIdx === idx ? jiggleAnimation : {}}
+            height={`${height}%`}
+            onClick={() => handleUdderClick(idx)}
+          />
+        ))}
       </UdderContainer>
-
-
-
     </Overlay>
   );
 };
